fd12323a21c687e892e73a81dfa765203a9380f5
harshu-202 <harshvardhanshirke202@gmail.com>
2024-08-26 15:51:33 +0530

    Add files via upload

---
+++ Logic Building/Assignments/Assign 1/Q1/positive.java
@@ -0,0 +1,11 @@
+class positive{
+	public static void main(String args[]){
+		int n=2;
+	if (n>0){
+	 System.out.println("The number is positive");
+}
+	else{
+	 System.out.println("The number is negative");
+}
+ }
+}
